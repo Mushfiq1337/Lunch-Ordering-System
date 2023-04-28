f7c208ef3067e18e52f39b652b1ea7f8044dc9cf
Mushfiq1337 <74034015+Mushfiq1337@users.noreply.github.com>
2023-04-28 08:12:17 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,100 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./App.css";
 
-function LunchOrderingSystem() {
-  // State variables
-  const [menuItems, setMenuItems] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [deliveryOption, setDeliveryOption] = useState('pickup');
-  const [paymentOption, setPaymentOption] = useState('creditCard');
-  const [userType, setUserType] = useState('employee');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
- }
-  // Load menu items from API on component mount
-  useEffect(() => {
-    axios.get('/api/menuItems')
-      .then(response => setMenuItems(response.data))
-      .catch(error => console.error(error));
-  }, []);
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState("employee");
 
-  // Handle adding/removing items from selectedItems array
-  function handleItemSelection(itemId) {
-    const newSelectedItems = [...selectedItems];
-    const itemIndex = newSelectedItems.indexOf(itemId);
-    if (itemIndex === -1) {
-      newSelectedItems.push(itemId);
-    } else {
-      newSelectedItems.splice(itemIndex, 1);
-    }
-    setSelectedItems(newSelectedItems);
-  }
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
-  // Handle submitting order to API
-  function handleSubmitOrder(event) {
-    event.preventDefault();
-    axios.post('/api/orders', {
-      items: selectedItems,
-      delivery: deliveryOption,
-      payment: paymentOption
-    })
-      .then(response => console.log(response.data))
-      .catch(error => console.error(error));
-  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-  // Handle user authentication
-  function handleLogin(event) {
-    event.preventDefault();
-    axios.post('/api/login', {
-      username: username,
-      password: password
-    })
-      .then(response => {
-        setIsLoggedIn(true);
-        setUsername('');
-        setPassword('');
-      })
-      .catch(error => console.error(error));
-  }
+  const handleUserTypeChange = (e) => {
+    setUserType(e.target.value);
+  };
 
-  // Conditional rendering based on user type and login status
-  let content = null;
-  if (isLoggedIn && userType === 'employee') {
-    content = (
-      <div>
-        <h2>Welcome Employee!</h2>
-        <form onSubmit={handleSubmitOrder}>
-          <h3>Menu Items:</h3>
-          {menuItems.map(item => (
-            <div key={item.id}>
-              <label>
-                <input type="checkbox" checked={selectedItems.includes(item.id)} onChange={() => handleItemSelection(item.id)} />
-                {item.name} (${item.price})
-              </label>
-            </div>
-          ))}
-          <h3>Delivery Option:</h3>
-          <label>
-            <input type="radio" name="deliveryOption" value="pickup" checked={deliveryOption === 'pickup'} onChange={() => setDeliveryOption('pickup')} />
-            Pick up
-          </label>
-          <label>
-            <input type="radio" name="deliveryOption" value="delivery" checked={deliveryOption === 'delivery'} onChange={() => setDeliveryOption('delivery')} />
-            Delivery
-          </label>
-          <h3>Payment Option:</h3>
-          <label>
-            <input type="radio" name="paymentOption" value="creditCard" checked={paymentOption === 'creditCard'} onChange={() => setPaymentOption('creditCard')} />
-            Credit Card
-          </label>
-          <label>
-            <input type="radio" name="paymentOption" value="debitCard" checked={paymentOption === 'debitCard'} onChange={() => setPaymentOption('debitCard')} />
-            Debit Card
-          </label>
-          <button type="submit">Submit Order</button>
-        </form>
-      </div>
-        );
-    }
-      export default LunchOrderingSystem;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}`);
+    console.log(`Email: ${email}`);
+    console.log(`User Type: ${userType}`);
+    // Call API to submit lunch order
+  };
+
+  return (
+    <div className="App">
+      <h1>Lunch Order System</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="userType">User Type:</label>
+          <select id="userType" onChange={handleUserTypeChange} value={userType}>
+            <option value="employee">Employee</option>
+            <option value="visitor">Visitor</option>
+            <option value="intern">Intern</option>
+          </select>
+        </div>
+        <button type="submit">Submit Order</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
